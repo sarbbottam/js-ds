875c56c6979b9dbbc0609dd597a715e6abefc9ce
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-05-05 16:39:42 -0700

    test(queue): granular test blocks

--- test/queue.js
+++ test/queue.js
@@ -1,26 +1,60 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const Queue = require('../src/queue');
 
-const queue = new Queue();
+let queue;
 
 describe('Queue', () => {
-  it('operations, isEmpty, getFront, enqueue, dequeue, clear', () => {
-    assert.ok(queue.isEmpty());
+  beforeEach(() => {
+    queue = new Queue();
+  });
 
+  it('should return true if the queue is empty', () => {
+    assert.isOk(queue.isEmpty());
+  });
+
+  it('should not enqueue if called w/o a value', () => {
     queue.enqueue();
+    assert.isNotOk(queue.getFront());
+  });
+
+  it('should not enqueue at the end of the queue', () => {
     queue.enqueue(1);
     queue.enqueue(2);
     queue.enqueue(3);
+    assert.equal(queue.dequeue(), 1);
+    assert.equal(queue.dequeue(), 2);
+    assert.equal(queue.dequeue(), 3);
+  });
 
-    assert.equal(queue.getFront(), 1);
+  it('should return falsy value on dequeue if the queue is empty', () => {
+    assert.isNotOk(queue.dequeue());
+  });
 
-    queue.dequeue();
-    assert.equal(queue.getFront(), 2);
+  it('should dequeue from the start of the queue', () => {
+    queue.enqueue(3);
+    queue.enqueue(2);
+    queue.enqueue(1);
+    assert.equal(queue.dequeue(), 3);
+    assert.equal(queue.dequeue(), 2);
+    assert.equal(queue.dequeue(), 1);
+  });
 
-    queue.dequeue();
-    assert.equal(queue.getFront(), 3);
-
+  it('should clear the queue', () => {
+    queue.enqueue(3);
+    queue.enqueue(2);
+    queue.enqueue(1);
     queue.clear();
-    assert.equal(queue.getFront(), undefined);
+    assert.isOk(queue.isEmpty());
+  });
+
+  it('should return falsy value for getFront if the queue is empty', () => {
+    assert.isNotOk(queue.getFront());
+  });
+
+  it('should return the front value for getFront if the queue is not empty', () => {
+    queue.enqueue(3);
+    queue.enqueue(2);
+    queue.enqueue(1);
+    assert.equal(queue.getFront(), 3);
   });
 });
