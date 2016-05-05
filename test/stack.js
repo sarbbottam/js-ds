f1d1f667e5928575f7ce36c3b8c69e2594b3c598
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-05-05 16:28:09 -0700

    test(stack): granular test blocks

--- test/stack.js
+++ test/stack.js
@@ -1,14 +1,35 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const Stack = require('../src/stack');
 
-const stack = new Stack();
+let stack;
 
 describe('Stack', () => {
-  it('operations, isEmpty, push, pop', () => {
-    assert.ok(stack.isEmpty());
-    stack.push(1);
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('should return true if the stack is empty', () => {
+    assert.isOk(stack.isEmpty());
+  });
+
+  it('should not push if called w/o a value', () => {
     stack.push();
+    assert.isNotOk(stack.pop());
+  });
+
+  it('should push to stack if called with a value', () => {
+    stack.push(1);
     assert.equal(stack.pop(), 1);
-    assert.equal(stack.pop(), undefined);
+  });
+
+  it('should pop the value from the top of the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    assert.equal(stack.pop(), 3);
+    assert.equal(stack.pop(), 2);
+    assert.equal(stack.pop(), 1);
+    assert.isNotOk(stack.pop());
+    assert.isOk(stack.isEmpty());
   });
 });
